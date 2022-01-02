6f3ae7251de556c1c83bbd2ce9ef50a33f393214
DostyTheGreatKigali <abumucal89@gmail.com>
2022-01-02 20:05:55 +0000

    Session storage added but login not working. Next code fixes it

--- structure-b4-local-and-session-storage/App.js
+++ structure-b4-local-and-session-storage/App.js
@@ -1,5 +1,4 @@
-// import React, { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,19 +6,8 @@ import Dashboard from '../src/components/Dashboard/Dashboard'
 import Preferences from '../src/components/Preferences/Preferences'
 import Login from '../src/components/Login/Login'
 
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
-
 function App() {
-  const token = getToken();
-  // const [token, setToken] = useState();
+  const [token, setToken] = useState();
 
   if(!token) {
     return <Login setToken={setToken} />
